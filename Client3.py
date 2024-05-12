03a3a01a571b3a74814f2ff5bee43e41e8c3f709
SorayaBR <soraya.bellmont@estudiants.urv.cat>
2024-05-12 17:38:51 +0200

    Scripts para ejecutar 5 clientes a la vez

---
+++ Client3.py
@@ -0,0 +1,3 @@
+from Cliente import run
+if __name__ == '__main__':
+    run()
---
+++ EjecutarClientes.py
@@ -0,0 +1,7 @@
+import subprocess
+
+subprocess.Popen(['start', 'cmd', '/k', 'python', 'Client1.py'], shell=True)
+subprocess.Popen(['start', 'cmd', '/k', 'python', 'Client2.py'], shell=True)
+subprocess.Popen(['start', 'cmd', '/k', 'python', 'Client3.py'], shell=True)
+subprocess.Popen(['start', 'cmd', '/k', 'python', 'Client4.py'], shell=True)
+subprocess.Popen(['start', 'cmd', '/k', 'python', 'Client5.py'], shell=True)
